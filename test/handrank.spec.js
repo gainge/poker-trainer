2dd828dcd39aa83fa6ec6cb1a5cf71fe9239edbc
Gibson Ainge <gainge@gmail.com>
2025-11-18 12:49:20 -0700

    remove unused spec file

--- test/handrank.spec.js
+++
@@ -1,6 +0,0 @@
-import {RANKS, SUITS} from '../src/model.js';
-import {it} from './testutil.test.js';
-
-it('should correctly identify a Royal Flush', () => {
-    // TODO: Implement test logic for Royal Flush
-});
